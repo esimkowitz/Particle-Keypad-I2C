e8ecac63f8fa6f12cd4896020043e2b96efe20ba
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2016-05-19 20:31:23 -0400

    Update Keypad.h

--- firmware/Keypad.h
+++ firmware/Keypad.h
@@ -7,23 +7,24 @@
 ||
 || @description
 || | This library provides a simple interface for using matrix
-|| | keypads. It supports multiple keypresses while maintaining
-|| | backwards compatibility with the old single key library.
-|| | It also supports user selectable pins and definable keymaps.
+|| | keypads over an I2C interface. It supports multiple 
+|| | keypresses while maintaining backwards compatibility with 
+|| | the old single key library. It also supports user selectable 
+|| | pins and definable keymaps.
 || #
 ||
 || @license
 || | This library is free software; you can redistribute it and/or
-|| | modify it under the terms of the GNU Lesser General Public
+|| | modify it under the terms of the GNU General Public
 || | License as published by the Free Software Foundation; version
-|| | 2.1 of the License.
+|| | 3 of the License.
 || |
 || | This library is distributed in the hope that it will be useful,
 || | but WITHOUT ANY WARRANTY; without even the implied warranty of
 || | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-|| | Lesser General Public License for more details.
+|| | General Public License for more details.
 || |
-|| | You should have received a copy of the GNU Lesser General Public
+|| | You should have received a copy of the GNU General Public
 || | License along with this library; if not, write to the Free Software
 || | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 || #
@@ -36,6 +37,8 @@
 #include "Key.h"
 
 #include "application.h"
+
+#include "Adafruit_MCP23008.h"
 
 #define OPEN LOW
 #define CLOSED HIGH
