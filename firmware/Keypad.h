dab5c783a6cb59762b711d293e726d7817f08c84
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2016-05-19 20:36:07 -0400

    Update Keypad.h

--- firmware/Keypad.h
+++ firmware/Keypad.h
@@ -1,9 +1,9 @@
 /*
 ||
-|| @file Keypad.h
-|| @version 3.1
-|| @author Mark Stanley, Alexander Brevig
-|| @contact mstanley@technologist.com, alexanderbrevig@gmail.com
+|| @file Keypad-I2C.h
+|| @version 0.1.0
+|| @author Mark Stanley, Alexander Brevig, Evan Simkowitz
+|| @contact mstanley@technologist.com, alexanderbrevig@gmail.com, esimkowitz@wustl.edu
 ||
 || @description
 || | This library provides a simple interface for using matrix
@@ -113,30 +113,6 @@ private:
 
 /*
 || @changelog
-|| | 3.1 2013-01-15 - Mark Stanley     : Fixed missing RELEASED & IDLE status when using a single key.
-|| | 3.0 2012-07-12 - Mark Stanley     : Made library multi-keypress by default. (Backwards compatible)
-|| | 3.0 2012-07-12 - Mark Stanley     : Modified pin functions to support Keypad_I2C
-|| | 3.0 2012-07-12 - Stanley & Young  : Removed static variables. Fix for multiple keypad objects.
-|| | 3.0 2012-07-12 - Mark Stanley     : Fixed bug that caused shorted pins when pressing multiple keys.
-|| | 2.0 2011-12-29 - Mark Stanley     : Added waitForKey().
-|| | 2.0 2011-12-23 - Mark Stanley     : Added the public function keyStateChanged().
-|| | 2.0 2011-12-23 - Mark Stanley     : Added the private function scanKeys().
-|| | 2.0 2011-12-23 - Mark Stanley     : Moved the Finite State Machine into the function getKeyState().
-|| | 2.0 2011-12-23 - Mark Stanley     : Removed the member variable lastUdate. Not needed after rewrite.
-|| | 1.8 2011-11-21 - Mark Stanley     : Added test to determine which header file to compile,
-|| |                                          WProgram.h or Arduino.h.
-|| | 1.8 2009-07-08 - Alexander Brevig : No longer uses arrays
-|| | 1.7 2009-06-18 - Alexander Brevig : This library is a Finite State Machine every time a state changes
-|| |                                          the keypadEventListener will trigger, if set
-|| | 1.7 2009-06-18 - Alexander Brevig : Added setDebounceTime setHoldTime specifies the amount of
-|| |                                          microseconds before a HOLD state triggers
-|| | 1.7 2009-06-18 - Alexander Brevig : Added transitionTo
-|| | 1.6 2009-06-15 - Alexander Brevig : Added getState() and state variable
-|| | 1.5 2009-05-19 - Alexander Brevig : Added setHoldTime()
-|| | 1.4 2009-05-15 - Alexander Brevig : Added addEventListener
-|| | 1.3 2009-05-12 - Alexander Brevig : Added lastUdate, in order to do simple debouncing
-|| | 1.2 2009-05-09 - Alexander Brevig : Changed getKey()
-|| | 1.1 2009-04-28 - Alexander Brevig : Modified API, and made variables private
-|| | 1.0 2007-XX-XX - Mark Stanley : Initial Release
+|| | 0.1 2016-5-19 - Evan Simkowitz	: Added declaration for Adafruit_MCP23008.h
 || #
 */
