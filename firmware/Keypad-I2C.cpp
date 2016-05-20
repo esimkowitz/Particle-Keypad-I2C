57d6b018a2da6315f7865d1a777f0e4614d1e83b
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2016-05-19 20:45:43 -0400

    Update and rename Keypad.cpp to Keypad-I2C.cpp

--- firmware/Keypad-I2C.cpp
+++ firmware/Keypad-I2C.cpp
@@ -1,38 +1,40 @@
 /*
 ||
 || @file Keypad.cpp
-|| @version 3.1
-|| @author Mark Stanley, Alexander Brevig
-|| @contact mstanley@technologist.com, alexanderbrevig@gmail.com
+|| @version 0.1.0
+|| @author Mark Stanley, Alexander Brevig, Evan Simkowitz
+|| @contact mstanley@technologist.com, alexanderbrevig@gmail.com, esimkowitz@wustl.edu
 ||
 || @description
 || | This library provides a simple interface for using matrix
-|| | keypads. It supports multiple keypresses while maintaining
-|| | backwards compatibility with the old single key library.
-|| | It also supports user selectable pins and definable keymaps.
+|| | keypads over an I2C interface. It supports multiple 
+|| | keypresses while maintaining backwards compatibility with 
+|| | the old single key library. It also supports user selectable 
+|| | pins and definable keymaps.
 || #
 ||
 || @license
 || | This library is free software; you can redistribute it and/or
-|| | modify it under the terms of the GNU Lesser General Public
+|| | modify it under the terms of the GNU General Public
 || | License as published by the Free Software Foundation; version
-|| | 2.1 of the License.
+|| | 3 of the License.
 || |
 || | This library is distributed in the hope that it will be useful,
 || | but WITHOUT ANY WARRANTY; without even the implied warranty of
 || | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-|| | Lesser General Public License for more details.
+|| | General Public License for more details.
 || |
-|| | You should have received a copy of the GNU Lesser General Public
+|| | You should have received a copy of the GNU General Public
 || | License along with this library; if not, write to the Free Software
 || | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 || #
 ||
 */
-#include "Keypad.h"
+
+#include "Keypad-I2C.h"
 
 // <<constructor>> Allows custom keymap, pin configuration, and keypad sizes.
-Keypad::Keypad(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols) {
+Keypad-I2C::Keypad-I2C(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols) {
 	rowPins = row;
 	columnPins = col;
 	sizeKpd.rows = numRows;
@@ -49,12 +51,12 @@ Keypad::Keypad(char *userKeymap, byte *row, byte *col, byte numRows, byte numCol
 }
 
 // Let the user define a keymap - assume the same row/column count as defined in constructor
-void Keypad::begin(char *userKeymap) {
+void Keypad-I2C::begin(char *userKeymap) {
     keymap = userKeymap;
 }
 
 // Returns a single key only. Retained for backwards compatibility.
-char Keypad::getKey() {
+char Keypad-I2C::getKey() {
 	single_key = true;
 
 	if (getKeys() && key[0].stateChanged && (key[0].kstate==PRESSED))
@@ -66,7 +68,7 @@ char Keypad::getKey() {
 }
 
 // Populate the key list.
-bool Keypad::getKeys() {
+bool Keypad-I2C::getKeys() {
 	bool keyActivity = false;
 
 	// Limit how often the keypad is scanned. This makes the loop() run 10 times as fast.
@@ -80,7 +82,7 @@ bool Keypad::getKeys() {
 }
 
 // Private : Hardware scan
-void Keypad::scanKeys() {
+void Keypad-I2C::scanKeys() {
 	// Re-intialize the row pins. Allows sharing these pins with other hardware.
 	for (byte r=0; r<sizeKpd.rows; r++) {
 		pin_mode(rowPins[r],INPUT_PULLUP);
@@ -100,7 +102,7 @@ void Keypad::scanKeys() {
 }
 
 // Manage the list without rearranging the keys. Returns true if any keys on the list changed state.
-bool Keypad::updateList() {
+bool Keypad-I2C::updateList() {
 
 	bool anyActivity = false;
 
@@ -149,7 +151,7 @@ bool Keypad::updateList() {
 
 // Private
 // This function is a state machine but is also used for debouncing the keys.
-void Keypad::nextKeyState(byte idx, boolean button) {
+void Keypad-I2C::nextKeyState(byte idx, boolean button) {
 	key[idx].stateChanged = false;
 
 	switch (key[idx].kstate) {
@@ -175,7 +177,7 @@ void Keypad::nextKeyState(byte idx, boolean button) {
 }
 
 // New in 2.1
-bool Keypad::isPressed(char keyChar) {
+bool Keypad-I2C::isPressed(char keyChar) {
 	for (byte i=0; i<LIST_MAX; i++) {
 		if ( key[i].kchar == keyChar ) {
 			if ( (key[i].kstate == PRESSED) && key[i].stateChanged )
@@ -187,7 +189,7 @@ bool Keypad::isPressed(char keyChar) {
 
 // Search by character for a key in the list of active keys.
 // Returns -1 if not found or the index into the list of active keys.
-int Keypad::findInList (char keyChar) {
+int Keypad-I2C::findInList (char keyChar) {
 	for (byte i=0; i<LIST_MAX; i++) {
 		if (key[i].kchar == keyChar) {
 			return i;
@@ -198,7 +200,7 @@ int Keypad::findInList (char keyChar) {
 
 // Search by code for a key in the list of active keys.
 // Returns -1 if not found or the index into the list of active keys.
-int Keypad::findInList (int keyCode) {
+int Keypad-I2C::findInList (int keyCode) {
 	for (byte i=0; i<LIST_MAX; i++) {
 		if (key[i].kcode == keyCode) {
 			return i;
@@ -208,43 +210,43 @@ int Keypad::findInList (int keyCode) {
 }
 
 // New in 2.0
-char Keypad::waitForKey() {
+char Keypad-I2C::waitForKey() {
 	char waitKey = NO_KEY;
 	while( (waitKey = getKey()) == NO_KEY );	// Block everything while waiting for a keypress.
 	return waitKey;
 }
 
 // Backwards compatibility function.
-KeyState Keypad::getState() {
+KeyState Keypad-I2C::getState() {
 	return key[0].kstate;
 }
 
 // The end user can test for any changes in state before deciding
 // if any variables, etc. needs to be updated in their code.
-bool Keypad::keyStateChanged() {
+bool Keypad-I2C::keyStateChanged() {
 	return key[0].stateChanged;
 }
 
 // The number of keys on the key list, key[LIST_MAX], equals the number
 // of bytes in the key list divided by the number of bytes in a Key object.
-byte Keypad::numKeys() {
+byte Keypad-I2C::numKeys() {
 	return sizeof(key)/sizeof(Key);
 }
 
 // Minimum debounceTime is 1 mS. Any lower *will* slow down the loop().
-void Keypad::setDebounceTime(uint debounce) {
+void Keypad-I2C::setDebounceTime(uint debounce) {
 	debounce<1 ? debounceTime=1 : debounceTime=debounce;
 }
 
-void Keypad::setHoldTime(uint hold) {
+void Keypad-I2C::setHoldTime(uint hold) {
     holdTime = hold;
 }
 
-void Keypad::addEventListener(void (*listener)(char)){
+void Keypad-I2C::addEventListener(void (*listener)(char)){
 	keypadEventListener = listener;
 }
 
-void Keypad::transitionTo(byte idx, KeyState nextState) {
+void Keypad-I2C::transitionTo(byte idx, KeyState nextState) {
 	key[idx].kstate = nextState;
 	key[idx].stateChanged = true;
 
@@ -266,28 +268,8 @@ void Keypad::transitionTo(byte idx, KeyState nextState) {
 
 /*
 || @changelog
-|| | 3.1 2013-01-15 - Mark Stanley     : Fixed missing RELEASED & IDLE status when using a single key.
-|| | 3.0 2012-07-12 - Mark Stanley     : Made library multi-keypress by default. (Backwards compatible)
-|| | 3.0 2012-07-12 - Mark Stanley     : Modified pin functions to support Keypad_I2C
-|| | 3.0 2012-07-12 - Stanley & Young  : Removed static variables. Fix for multiple keypad objects.
-|| | 3.0 2012-07-12 - Mark Stanley     : Fixed bug that caused shorted pins when pressing multiple keys.
-|| | 2.0 2011-12-29 - Mark Stanley     : Added waitForKey().
-|| | 2.0 2011-12-23 - Mark Stanley     : Added the public function keyStateChanged().
-|| | 2.0 2011-12-23 - Mark Stanley     : Added the private function scanKeys().
-|| | 2.0 2011-12-23 - Mark Stanley     : Moved the Finite State Machine into the function getKeyState().
-|| | 2.0 2011-12-23 - Mark Stanley     : Removed the member variable lastUdate. Not needed after rewrite.
-|| | 1.8 2011-11-21 - Mark Stanley     : Added decision logic to compile WProgram.h or Arduino.h
-|| | 1.8 2009-07-08 - Alexander Brevig : No longer uses arrays
-|| | 1.7 2009-06-18 - Alexander Brevig : Every time a state changes the keypadEventListener will trigger, if set.
-|| | 1.7 2009-06-18 - Alexander Brevig : Added setDebounceTime. setHoldTime specifies the amount of
-|| |                                          microseconds before a HOLD state triggers
-|| | 1.7 2009-06-18 - Alexander Brevig : Added transitionTo
-|| | 1.6 2009-06-15 - Alexander Brevig : Added getState() and state variable
-|| | 1.5 2009-05-19 - Alexander Brevig : Added setHoldTime()
-|| | 1.4 2009-05-15 - Alexander Brevig : Added addEventListener
-|| | 1.3 2009-05-12 - Alexander Brevig : Added lastUdate, in order to do simple debouncing
-|| | 1.2 2009-05-09 - Alexander Brevig : Changed getKey()
-|| | 1.1 2009-04-28 - Alexander Brevig : Modified API, and made variables private
-|| | 1.0 2007-XX-XX - Mark Stanley : Initial Release
+|| | 0.1 2016-5-19 - Evan Simkowitz	: Accounted for name change of Keypad-I2C.h from Keypad.h
+|| | 0.1 2016-5-19 - Evan Simkowitz	: Changed name from Keypad.cpp to Keypad-I2C.cpp
+|| | 0.1 2016-5-19 - Evan Simkowitz	: Added the Adafruit_MCP23008.h
 || #
 */
